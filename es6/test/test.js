ee199ee406124ef46568ce33b23084d1d59f34fe
wang0930 <2269055154@qq.com>
2019-08-08 14:35:41 +0800

    200

---
+++ es6/test/test.js
@@ -0,0 +1,14 @@
+function person(name,age){
+    this.name=name;
+    this.age=age;
+}
+person.prototype.say = function(){
+    console.log('i am '+this.name+ ' i am '+this.age);
+}
+var p1=new person('wyb',21);
+     p1.say();
+     function coder(name,age){
+         person.call(this, name,age);
+     }
+     var c1=new coder('wyb',21);
+     console.log(c1);
