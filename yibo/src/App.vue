d8a842e1dca841cdd46089e79103cb2d2cbcb09e
wang0930 <2269055154@qq.com>
2019-09-21 15:05:52 +0800

    vuecli

--- yibo/src/App.vue
+++ yibo/src/App.vue
@@ -1,29 +1,54 @@
+<!-- 总组件 -->
 <template>
   <div id="app">
+    <!-- <common-header></common-header> -->
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link>|
+      <!-- <router-link to="/mine">我的 </router-link>| -->
+      <!-- params传参 不能使用path直接传 需要使用name -->
+      <router-link :to="{name:'mine',params:{id:666,name:'小王'}}">我的params </router-link>|
+      <!-- query传参 -->
+      <router-link :to="{path:'/mine',query:{id:666,name:'小王'}}">我的query </router-link>|
+      <!-- 通过路径传参 -->
+      <router-link to="/test/5/lisi">test</router-link> |
+      <router-link to="/aaa">跳转aaa</router-link> |
+      <router-link to="/c">跳转c </router-link>
     </div>
-    <router-view/>
+    <transition name="fade">
+       <router-view/>
+    </transition>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+    // 1 引入  2components下声明
+    import CommonHeader from './components/CommonHeader';
+    export default {
+        components:{
+            CommonHeader
+        }
     }
+</script>
+
+<style lang="scss">
+  .fade-enter{
+    opacity: 0;
   }
-}
+  .fade-enter-active{
+    transition:opacity 2s ease;
+  }
+  .fade-enter-to{
+    opacity: 1;
+  }
+  .fade-leave{
+    transform: translateX(0);
+  }
+  .fade-leave-active{
+    transition: transform 1s ease;
+  }
+  .fade-leave-to{
+    transform: translateX(100%);
+  }
 </style>
+
--- yibo/src/router.js
+++ yibo/src/router.js
@@ -1,15 +1,23 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Mine from './views/Mine';
+import Test from './views/Test';
+import Test1 from './views/Test1';
+import Test2 from './views/Test2';
+import Error from './views/Error'
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home,
+      alias:'/c'
     },
     {
       path: '/about',
@@ -19,5 +27,34 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+    {
+      path:'/mine',
+      name:'mine',
+      component:Mine,
+      children:[
+          {
+            path:'test1',
+            name:'test1',
+            component:Test1
+          },
+          {
+            path:'test2',
+            name:'test2',
+            component:Test2
+          }
+      ]
+    },{
+      path:'/test/:id/:name',
+      name:'test',
+      component:Test
+    },{
+      path:'/aaa',
+      redirect:'/'
+    }
+    ,{
+      path:'*',
+      name:'error',
+      component:Error
+    }
   ],
 });
